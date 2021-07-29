f483f66580fc51a29cf7af2c03810b3b9757f973
Anil Cokbilir <anil-27@hotmail.de>
2021-07-29 12:40:41 +0200

    header

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['munich1.jpg', 'munich2.jpg', 'munich3.jpg'];
+  headlines = ['Bring engineering to the next level', 'Born to code', 'Be the best'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
+}
